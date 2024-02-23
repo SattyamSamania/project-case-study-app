a783c98294908129f84627c960123a2225626ff6
SattyamSamania <Sattyamsamania2810@gmail.com>
2024-02-23 17:08:24 +0530

    Add Routes for Notes

---
+++ server/routes/note-routes.js
@@ -0,0 +1,12 @@
+// routes/note-rotes.js
+const express = require('express')
+
+const noteController = require('../controllers/noteControllers')
+
+const router = express.Router()
+
+
+router.post('/', noteController.createItem)
+router.get('/', noteController.getNotes)
+
+module.exports = router;
