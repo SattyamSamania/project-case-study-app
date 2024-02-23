f14f3ced111b4ee9051a2e5030d9511d1831fb7c
SattyamSamania <Sattyamsamania2810@gmail.com>
2024-02-23 17:21:14 +0530

    Bug fixed in noteControllers

--- server/controllers/noteControllers.js
+++ server/controllers/noteControllers.js
@@ -35,17 +35,15 @@ createItem = (req, res) => {
  
 
 getNotes = async (req, res) => {
-    await Note.find({}, (err, notes) => {
-        if (err) {
-            return res.status(400).json({ success: false, error: err })
-        }
+    try {
+        const notes = await Note.find({});
         if (!notes.length) {
-            return res
-                .status(404)
-                .json({ success: false, error: `Sorry, Item not found` })
+            return res.status(404).json({ success: false, error: 'Sorry, Item not found' });
         }
-        return res.status(200).json({ success: true, data: notes })
-    }).catch(err => console.log(err))
-}
+        return res.status(200).json({ success: true, data: notes });
+    } catch (err) {
+        return res.status(400).json({ success: false, error: err.message });
+    }
+};
 
 module.exports = {createItem, getNotes}
